e7a5d31611ec5da6317957b34f1d9d43d24961b2
jh3y <jh3y@users.noreply.github.com>
2024-07-05 00:00:58 +0100

    start switching up parachute bears

--- src/pages/demos/parachute-bears/_script.js
+++ src/pages/demos/parachute-bears/_script.js
@@ -1,66 +1,61 @@
-import { gsap } from "gsap"
-import { Draggable } from "gsap/Draggable"
-import { Physics2DPlugin } from "gsap/Physics2DPlugin"
-import getBear from "./_getBear.js"
+import { gsap } from "gsap";
+import { Draggable } from "gsap/Draggable";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+import getBear from "./_getBear.js";
 
-console.clear()
+console.clear();
 
-gsap.registerPlugin(Draggable)
-gsap.registerPlugin(Physics2DPlugin)
+gsap.registerPlugin(Draggable);
+gsap.registerPlugin(Physics2DPlugin);
 
 // let open = false;
-let count = 0
+let count = 0;
 
-const MENU = document.querySelector(".menu")
-const POPOVER = MENU.querySelector("[popover]")
-const PEN = MENU.querySelector(".parachute-pen")
-const HANDLE = MENU.querySelector(".bear--handle")
+const MENU = document.querySelector(".menu");
+const POPOVER = MENU.querySelector("[popover]");
+const PEN = MENU.querySelector(".parachute-pen");
+const HANDLE = MENU.querySelector(".bear--handle");
 // const LIST = POPOVER.querySelector(".option-box");
-const OPTIONS = POPOVER.querySelector(".options")
+const OPTIONS = POPOVER.querySelector(".options");
 // const SELECTBUTTON = MENU.querySelector("button");
-const NOSEY_PEN = MENU.querySelector(".nosey-pen")
-const PROXY = document.createElement("div")
+const NOSEY_PEN = MENU.querySelector(".nosey-pen");
+const PROXY = document.createElement("div");
 
-const SNAP_THRESHOLD = 200
-let DISTANCE_PER_SECOND
-
-const POP = new Audio("/audio/pop.mp3")
-const R2_SCREAM = new Audio("/audio/r2d2_scream.mp3")
-
-const SPEED_SCALE = gsap.utils.mapRange(0, SNAP_THRESHOLD, 1, 0.3)
+const POP = new Audio("/audio/pop.mp3");
+const R2_SCREAM = new Audio("/audio/r2d2_scream.mp3");
 
 const RESET = () => {
   gsap.set(PROXY, {
     x: 0,
     y: 0,
-  })
-}
+  });
+};
 
 const yeet = ({ bear, dps }) => {
   gsap.to(bear.firstChild, {
     y: -window.innerHeight,
     duration: window.innerHeight / dps,
     onStart: () => {
-      R2_SCREAM.pause()
-      R2_SCREAM.currentTime = 0
-      R2_SCREAM.play()
+      R2_SCREAM.pause();
+      R2_SCREAM.currentTime = 0;
+      R2_SCREAM.play();
     },
     onComplete: () => {
-      bear.remove()
+      bear.remove();
     },
-  })
-}
+  });
+};
 
 const launch = ({ bear, ejector, distance, velocity }) => {
-  POP.pause()
-  POP.currentTime = 0
-  POP.play()
-  const PARACHUTE = bear.querySelector(".parachute")
-  const BEAR_SVG = bear.querySelector(".bear")
-  const delay = gsap.utils.mapRange(300, 650, 0.5, 1)(velocity)
-  gsap.set(PARACHUTE, { scale: 0, transformOrigin: "50% 100%" })
+  POP.pause();
+  POP.currentTime = 0;
+  POP.play();
+  const PARACHUTE = bear.querySelector(".parachute");
+  const BEAR_SVG = bear.querySelector(".bear");
+  const delay = gsap.utils.mapRange(300, 650, 0.25, 0.75)(velocity);
+  gsap.set(PARACHUTE, { scale: 0, transformOrigin: "50% 100%" });
   // Do a physics animations...
-  const left = Math.random() > 0.5
+  const left = Math.random() > 0.5;
   // At this point we need to make the parachuter jump but not the bear.
   // Start by clipping the bear and then unclip after peak jump...
   const JUMP = gsap
@@ -75,18 +70,18 @@ const launch = ({ bear, ejector, distance, velocity }) => {
       onStart: () => {
         gsap.set(bear, {
           clipPath: "inset(-100vh -100vh 100% -100vh)",
-        })
+        });
         gsap.delayedCall(delay, () => {
           gsap.set(bear, {
             clipPath: "none",
-          })
-        })
+          });
+        });
       },
       onComplete: () => {
-        bear.remove()
+        bear.remove();
       },
     })
-    .timeScale(2.2)
+    .timeScale(2.2);
   gsap.delayedCall(delay + gsap.utils.random(-0.2, 0.2), () => {
     gsap
       .timeline({
@@ -95,7 +90,7 @@ const launch = ({ bear, ejector, distance, velocity }) => {
             gsap.to(BEAR_SVG, {
               x: left ? -window.innerWidth : window.innerWidth,
               duration: 30,
-            })
+            });
           }
         },
       })
@@ -111,8 +106,8 @@ const launch = ({ bear, ejector, distance, velocity }) => {
           duration: 0.1,
         }),
         "<",
-      )
-  })
+      );
+  });
   // const distance = gsap.utils.random(
   //   window.innerHeight * 0.25,
   //   window.innerHeight * 0.5,
@@ -174,79 +169,89 @@ const launch = ({ bear, ejector, distance, velocity }) => {
   //     0,
   //   )
   //   .timeScale(3);
-}
+};
 
 // let snapped = false;
-let closing = false
-let duration = 0
-let hue = gsap.utils.random(0, 359, 1)
-let pos = gsap.utils.random(10, 60, 1)
-let rot = 0
-let tilt = 0
-MENU.style.setProperty("--pos", pos)
-HANDLE.innerHTML = getBear(false, "holdingOn")
-NOSEY_PEN.innerHTML = getBear(false, "holdingOn")
+let closing = false;
+let duration = 0;
+let hue = gsap.utils.random(0, 359, 1);
+let pos = gsap.utils.random(10, 60, 1);
+let rot = 0;
+let tilt = 0;
+MENU.style.setProperty("--pos", pos);
+HANDLE.innerHTML = getBear(false, "holdingOn");
+NOSEY_PEN.innerHTML = getBear(false, "holdingOn");
 
 POPOVER.addEventListener("toggle", ({ newState, oldState }) => {
   if (newState === "closed" && oldState === "open") {
     if (closing) {
-      closing = false
-      hue = gsap.utils.random(0, 359)
-      pos = gsap.utils.random(10, 60, 1)
-      rot = Math.random() > 0.5
-      tilt = gsap.utils.random(-8, 8)
-      MENU.style.setProperty("--hue", hue)
-      MENU.style.setProperty("--pos", pos)
-      MENU.style.setProperty("--rot", rot ? 1 : 0)
-      MENU.style.setProperty("--tilt", tilt)
+      closing = false;
+      hue = gsap.utils.random(0, 359);
+      pos = gsap.utils.random(-5, 45, 1);
+      rot = Math.random() > 0.5;
+      tilt = gsap.utils.random(-8, 8);
+      MENU.style.setProperty("--hue", hue);
+      MENU.style.setProperty("--pos", pos);
+      MENU.style.setProperty("--rot", rot ? 1 : 0);
+      MENU.style.setProperty("--tilt", tilt);
       setTimeout(() => {
-        OPTIONS.removeAttribute("style")
-        gsap.set(HANDLE, { clearProps: "transform" })
-        HANDLE.removeAttribute("style")
-      }, duration * 1000)
+        OPTIONS.removeAttribute("style");
+        gsap.set(HANDLE, { clearProps: "transform" });
+        HANDLE.removeAttribute("style");
+      }, duration * 1000);
     } else {
-      gsap.set(OPTIONS, { clearProps: "all" })
+      gsap.set(OPTIONS, { clearProps: "all" });
     }
   }
-})
+});
 
 // Used for pulling the Popover
 const onDrag = function () {
-  if (!this.__allowDrag) return
+  if (!this.__allowDrag) return;
   // Set the scale based on the position and popoverHeight
   gsap.set(HANDLE, {
     y: this.y,
-  })
+  });
   gsap.set(OPTIONS, {
-    scaleY: (this.__popoverHeight + this.y) / this.__popoverHeight,
-  })
-}
+    scaleY: Math.max(1, (this.__popoverHeight + this.y) / this.__popoverHeight),
+  });
+};
 
 // On Popover drag chcek some constraints, etc.
 const onDragStart = function ({ x, y }) {
-  const BOUNDS = POPOVER.getBoundingClientRect()
-  gsap.set(OPTIONS, { clearProps: "all" })
-  gsap.set(OPTIONS, { transition: "none" })
-  gsap.set(".options button", { pointerEvents: "none" })
-  this.__allowDrag = y > BOUNDS.bottom - 50
-  this.__popoverHeight = BOUNDS.height
-}
+  const BOUNDS = POPOVER.getBoundingClientRect();
+  gsap.set(OPTIONS, { clearProps: "all" });
+  gsap.set(OPTIONS, { transition: "none" });
+  gsap.set(".options button", { pointerEvents: "none" });
+  this.__allowDrag = y > BOUNDS.bottom - 50;
+  this.__popoverHeight = BOUNDS.height;
+};
 
+const SNAP_THRESHOLD = 200;
+let DISTANCE_PER_SECOND;
+const SPEED_SCALE = gsap.utils.mapRange(
+  SNAP_THRESHOLD * 0.5,
+  SNAP_THRESHOLD,
+  0.5,
+  0.25,
+);
 // This is where you either yeet/launch
 const onRelease = function () {
   // Reset the proxy handle
-  RESET()
-  this.__allowDrag = false
+  RESET();
+  this.__allowDrag = false;
 
-  duration = gsap.utils.clamp(0.5, 1, SPEED_SCALE(this.y))
+  duration = gsap.utils.clamp(0.25, 0.5, SPEED_SCALE(this.y));
 
-  // Spring back to default position if you haven't exceeded the catapult zone
+  console.info({ y: this.y, duration });
+
+  // 1. Bounce back when under threshold (Spring)
   if (this.y > 0 && this.y < SNAP_THRESHOLD / 2) {
     gsap
       .timeline({
         onComplete: () => {
-          POPOVER.removeAttribute("style")
-          gsap.set(".options button", { pointerEvents: "all" })
+          POPOVER.removeAttribute("style");
+          gsap.set(".options button", { pointerEvents: "all" });
         },
       })
       .to(HANDLE, {
@@ -262,30 +267,31 @@ const onRelease = function () {
           ease: "elastic.out(1, 0.3)",
         },
         0,
-      )
+      );
   } else if (
     this.y > 0 &&
     this.y >= SNAP_THRESHOLD / 2 &&
     POPOVER.matches(":popover-open")
   ) {
-    // THIS IS WHERE THE EJECTING LOGIC GOES...
-
+    // 2. Snap the popover :closed && initiate launch
     // Snap closed please
-    closing = true
-    count += 1
+    closing = true;
+    count += 1;
     // Duration * 1 / duration
     // distance per seconds = (1 / duration) * distance
-    DISTANCE_PER_SECOND = (1 / duration) * (this.__popoverHeight + this.y)
-    const DISTANCE = this.__popoverHeight * 1.5
+    // The DPS is necessary so the popover springs closed at the right rate
+
+    DISTANCE_PER_SECOND = (1 / duration) * (this.__popoverHeight + this.y);
+    const DISTANCE = this.__popoverHeight * (this.y / this.__popoverHeight + 1);
 
     // This timeline is for springing back the Popover to closed
     // But at the same time it needs to trigger the launch/yeet of the bear
     gsap
       .timeline({
         onStart: () => {
-          gsap.set(OPTIONS, { boxShadow: 0 })
-          // POPOVER.style.transition = '0s'
-          gsap.delayedCall(duration * 0.2, () => {
+          gsap.set(OPTIONS, { boxShadow: 0 });
+
+          gsap.delayedCall(duration * 0.5, () => {
             const BEAR = Object.assign(document.createElement("div"), {
               className: "bear__container",
               style: `--eye-delay: ${gsap.utils.random(
@@ -296,26 +302,29 @@ const onRelease = function () {
               innerHTML: `<div class="parachuter">
                 ${getBear(hue)}
               </div>`,
-            })
-            PEN.appendChild(BEAR)
+            });
+            PEN.appendChild(BEAR);
+
             const velocity = gsap.utils.mapRange(
               400,
               900,
               300,
               650,
-            )(DISTANCE_PER_SECOND)
+            )(DISTANCE_PER_SECOND);
 
             if (velocity >= 630 && Math.random() > 0.5 && count > 3) {
-              yeet({ bear: BEAR, dps: DISTANCE_PER_SECOND })
+              console.info("YEET");
+              // yeet({ bear: BEAR, dps: DISTANCE_PER_SECOND });
             } else {
-              launch({ bear: BEAR, distance: DISTANCE, velocity })
+              console.info("LAUNCH");
+              // launch({ bear: BEAR, distance: DISTANCE, velocity });
             }
-          })
+          });
         },
         onComplete: () => {
-          gsap.set(OPTIONS, { display: "none" })
-          gsap.set(".options button", { pointerEvents: "all" })
-          if (POPOVER.matches(":popover-open")) POPOVER.hidePopover()
+          gsap.set(OPTIONS, { display: "none" });
+          gsap.set(".options button", { pointerEvents: "all" });
+          if (POPOVER.matches(":popover-open")) POPOVER.hidePopover();
         },
       })
       .to(HANDLE, {
@@ -333,22 +342,22 @@ const onRelease = function () {
           duration: duration * 0.5,
         },
         0,
-      )
+      );
   }
-}
+};
 
 Draggable.create(PROXY, {
   trigger: OPTIONS,
   type: "x,y",
   onDrag: function () {
-    onDrag.bind(this)()
+    onDrag.bind(this)();
   },
   onDragStart: function (args) {
-    onDragStart.bind(this)(args)
+    onDragStart.bind(this)(args);
   },
   onRelease: function () {
-    onRelease.bind(this)()
+    onRelease.bind(this)();
   },
-})
+});
 
-gsap.set(MENU, { display: "block" })
+gsap.set(MENU, { display: "block" });
